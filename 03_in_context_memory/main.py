4aae3382c085e758968434aec476cdf523fd0f89
krasi <krasimir.xristov@gmail.com>
2026-06-22 22:18:25 +0200

    client

--- 03_in_context_memory/main.py
+++ 03_in_context_memory/main.py
@@ -2,6 +2,11 @@ import os
 
 from dotenv import load_dotenv
 from openai import OpenAI
-from openai import 
 
 load_dotenv()
+
+
+# OpenRouter Client
+client = OpenAI(
+    api_key=os.getenv("OPENROUTER_API_KEY"), base_url="https://openrouter.ai/api/v1"
+)
